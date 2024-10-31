26324304c4a9fb0ef2a12d53c4d557c34dd7f36d
camilinhaa <126702992+camilinhaa@users.noreply.github.com>
2024-10-31 11:27:49 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -18,9 +18,42 @@ async function quantidadeUsuariosPorRede() {
         }
       ]
 
-      const layout ={}
-       plot
-  
+      const layout ={
+        plot_bgcolor:getCSS('--bg-color'),
+        paper_bgcolor:getCSS('--bg-color'),
+        title:{
+             text: 'Redes sociais com mais usuários no mundo',
+             x: 0,
+             font:{
+                  color: getCSS('--primary-color'),
+                  family:getCSS('--font'),
+                  size:30
+
+             }
+        },
+         xaxis:{
+               tickfont{
+                
+               }
+              title:{
+                 text: 'nome das redes sociais',
+                 font:{
+                     color: getCSS('--secondary-color')
+                 }
+              }
+
+         },
+         yaxis:{
+          itle:{
+            text: 'bilhões de usuários ativos'
+            font:{
+                color: getCSS('--secondary-color')
+
+         }
+
+
+      }
+       
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
